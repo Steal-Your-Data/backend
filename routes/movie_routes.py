96afd9b71ac153511e9a5c54f6f7c3343090cda6
Rushil Sambangi <143136941+rsambangi@users.noreply.github.com>
2025-03-05 18:03:49 -0600

    Update movie_routes.py

--- routes/movie_routes.py
+++ routes/movie_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from model import Movie
 from extentions import db
+from sqlalchemy import extract
 movie_bp = Blueprint('movies', __name__)
 
 
@@ -45,6 +46,103 @@ def Get_info_id():
 
     return jsonify(result)
 
+@movie_bp.route('/get_all_movies', methods=['GET'])
+def get_all_movies():
+    movies = Movie.query.limit(10).all()  # REMOVE limit(10) to get all movies
+    result = []
+
+    for movie in movies:
+        result.append({
+            'id': movie.id,
+            'title': movie.title,
+            'genres': movie.genres,
+            'original_language': movie.original_language,
+            'overview': movie.overview,
+            'popularity': movie.popularity,
+            'release_date': movie.release_date.isoformat() if movie.release_date else None,
+            'poster_path': movie.poster_path
+        })
+
+    return jsonify(result)
+
+@movie_bp.route('/sort_movies', methods=['GET'])
+def sort_movies():
+    sort_by = request.args.get('sort_by', 'title')  # Default to sorting by title
+    order = request.args.get('order', 'asc').lower()  # Default to ascending order
+
+    valid_sort_columns = {
+        'popularity': Movie.popularity,
+        'release_date': Movie.release_date,
+        'title': Movie.title
+    }
+
+    if sort_by not in valid_sort_columns:
+        return jsonify({'error': 'Invalid sort field'}), 400
+
+    # Sort movies based on user request
+    query = Movie.query.order_by(
+        valid_sort_columns[sort_by].desc() if order == 'desc' else valid_sort_columns[sort_by].asc()
+    )
+
+    movies = query.all()
+    
+    result = [
+        {
+            'id': movie.id,
+            'title': movie.title,
+            'genres': movie.genres,
+            'original_language': movie.original_language,
+            'overview': movie.overview,
+            'popularity': movie.popularity,
+            'release_date': movie.release_date.isoformat() if movie.release_date else None,
+            'poster_path': movie.poster_path
+        }
+        for movie in movies
+    ]
+    
+    return jsonify(result)
+
+
+@movie_bp.route('/filter_movies', methods=['GET'])
+def filter_movies():
+    genres = request.args.getlist('genres')  # Expect a list of genres
+    language = request.args.get('language')
+    release_year = request.args.get('release_year', type=int)
+
+    query = Movie.query
+
+    # Filter by genres (matches at least one selected genre)
+    if genres:
+        genre_filters = [Movie.genres.ilike(f"%{genre}%") for genre in genres]
+        query = query.filter(db.or_(*genre_filters))
+
+    # Filter by language
+    if language:
+        query = query.filter(Movie.original_language == language)
+
+    # Filter by release year
+    if release_year:
+        query = query.filter(extract('year', Movie.release_date) == release_year)
+
+    movies = query.all()
+
+    result = [
+        {
+            'id': movie.id,
+            'title': movie.title,
+            'genres': movie.genres,
+            'original_language': movie.original_language,
+            'overview': movie.overview,
+            'popularity': movie.popularity,
+            'release_date': movie.release_date.isoformat() if movie.release_date else None,
+            'poster_path': movie.poster_path
+        }
+        for movie in movies
+    ]
+
+    return jsonify(result)
+
+
 @movie_bp.route('/get_movie_info_by_ids', methods=['GET'])
 def Get_info_ids():
     data = request.json
@@ -66,5 +164,3 @@ def Get_info_ids():
         results.append(result)
 
     return jsonify(results)
-
-
