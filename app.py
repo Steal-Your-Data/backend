1aecd272ce9da89bf9e827ff9190ccc2c735312f
Rushil Sambangi <143136941+rsambangi@users.noreply.github.com>
2025-03-05 18:04:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def create_app():
     CORS(app)
 
     # Configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/terry/Desktop/CS506/movies_v2.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////Users/rsamb/Downloads/backend/movies_v2.db"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     app.config['SECRET_KEY'] = 'your_secret_key'
